bad54ca3904266c2ed060b4f89ddeff4b255331b
win0lin <test@example.com>
2017-10-29 08:15:25 +0100

    Hello Azure App Service

---
+++ js/my.js
@@ -0,0 +1,18 @@
+document.write('\
+<nav role="navigation">\
+  <div id="menuToggle">\
+    <input type="checkbox" />\
+    <span></span>\
+    <span></span>\
+    <span></span>\
+    <ul id="menu">\
+      <a href="index.html"><li>Home</li></a>\
+      <a href="myindex.html"><li>Index</li></a>\
+      <a href="#"><li>Info</li></a>\
+      <a href="#"><li>Contact</li></a>\
+      <a href="https://erikterwan.com/" target="_blank"><li>Show me more</li></a>\
+    </ul>\
+  </div>\
+</nav>\
+\
+');
---
+++ js/my1.js
@@ -0,0 +1,6 @@
+document.write('\
+<h1>Add your HTML code here</h1>\
+\
+<p>aaa</p>\
+\
+');
